499c36a3d9b34c1ad7b2bb2fe83420158e80a4b4
Pequito97 <Pequito97@users.noreply.github.com>
2018-10-14 21:02:03 +0100

    Add project files.

---
+++ TutorialMVC_160221034/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TutorialMVC_160221034.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
